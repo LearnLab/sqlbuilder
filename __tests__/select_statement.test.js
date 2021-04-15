60b7e87e1a851df1933dda5b0f69b8c842a6dbd0
Diego Castillo <0325.diego@gmail.com>
2021-04-14 21:24:09 -0500

    Add tests for the Select Statement

---
+++ __tests__/select_statement.test.js
@@ -0,0 +1,16 @@
+const SQLBuilder = require("../index");
+
+test('query.select().from(users).where(email, =, my_email) => SELECT * FROM users WHERE email=$1', () => {
+    const columns = [];
+    const tables = ['users'];
+    const values = ['my_email'];
+    const condition = ['email', '=', values[0]];
+
+    const query = SQLBuilder.select(...columns)
+                            .from(...tables)
+                            .where(...condition);
+
+    expect(query.selectStatement()).toEqual('SELECT * FROM users WHERE email=$1;');
+    expect(query.values).toEqual(values);
+});
+
