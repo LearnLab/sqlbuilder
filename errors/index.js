0f43b6348de7f9ba082417ee8b2a1defa636e5be
Diego Castillo <0325.diego@gmail.com>
2021-06-26 15:02:22 -0500

    Change error name for ALL_RECORDS

--- errors/index.js
+++ errors/index.js
@@ -3,7 +3,7 @@ const Errors = {
   REFERENCE_AND_SCALAR_REQUIRED: Error('Table reference and scalar values are required'),
   AS_MUCH_COLUMNS_AS_VALUES: Error('The set of columns must have the same length as the number of scalar values'),
   AT_LEAST_ONE_VALUE: Error('You have to insert at least 1 value in all records'),
-  ALL_RECORDS_SAME_LENGTH: Error('All scalar sets must have the same number of elements'),
+  ALL_ROWS_SAME_LENGTH: Error('All scalar sets must have the same number of elements'),
   NO_NESTED_VALUES: Error('You can not insert nested values'),
 };
 
