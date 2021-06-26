dc427248ba0851b618250f28027953513f5a0fa8
Diego Castillo <0325.diego@gmail.com>
2021-06-26 11:09:56 -0500

    Fix errors unit tests syntax and add Errors object for consistency

--- __tests__/insert_statement.test.js
+++ __tests__/insert_statement.test.js
@@ -1,4 +1,11 @@
 const SQLBuilder = require('../index');
+const {
+  REFERENCE_AND_SCALAR_REQUIRED,
+  AS_MUCH_COLUMNS_AS_VALUES,
+  AT_LEAST_ONE_VALUE,
+  ALL_RECORDS_SAME_LENGTH,
+  NO_NESTED_VALUES,
+} = require('../errors');
 
 /* Basic usage */
 test('query.insert(col1, col2).into(table).values(val1, val2) => INSERT INTO table (col1, col2) VALUES ($1, $2)', () => {
@@ -46,37 +53,37 @@ test('query.insert() requires a table reference and a set of values', () => {
   const noTable = SQLBuilder.insert().values('one, two');
   const noValues = SQLBuilder.insert().into('table');
 
-  expect(nothing.insertStatement()).toThrowError(Error('Table reference and scalar values required'));
-  expect(noTable.insertStatement()).toThrowError(Error('Table reference and scalar values required'));
-  expect(noValues.insertStatement()).toThrowError(Error('Table reference and scalar values required'));
+  expect(() => nothing.insertStatement()).toThrow(REFERENCE_AND_SCALAR_REQUIRED);
+  expect(() => noTable.insertStatement()).toThrow(REFERENCE_AND_SCALAR_REQUIRED);
+  expect(() => noValues.insertStatement()).toThrow(REFERENCE_AND_SCALAR_REQUIRED);
 });
 
 test('query.insert() requires the same number of column references as scalar values', () => {
   const moreColumns = SQLBuilder.insert('one', 'two').into('table').values('one');
   const moreValues = SQLBuilder.insert('one', 'two').into('table').values('one');
 
-  expect(moreColumns.values('one')).toThrowError(Error('The set of columns must have the same length as the number of scalar values'));
-  expect(moreValues.values('one', 'two', 'three')).toThrowError(Error('The set of columns must have the same length as the number of scalar values'));
+  expect(() => moreColumns.values('one')).toThrow(AS_MUCH_COLUMNS_AS_VALUES);
+  expect(() => moreValues.values('one', 'two', 'three')).toThrow(AS_MUCH_COLUMNS_AS_VALUES);
 });
 
 test('query.insert() prohibits to insert 0 values', () => {
   const emptyValues = SQLBuilder.insert().into('table');
   const emptyValuesArray = SQLBuilder.insert().into('table');
 
-  expect(emptyValues.insert()).toThrowError(Error('You have to insert at least 1 value in all records'));
-  expect(emptyValuesArray.insert(['one'], [], [])).toThrowError(Error('You have to insert at least 1 value in all records'));
+  expect(() => emptyValues.insert()).toThrow(AT_LEAST_ONE_VALUE);
+  expect(() => emptyValuesArray.insert(['one'], [], [])).toThrow(AT_LEAST_ONE_VALUE);
 });
 
 test('query.insert() requires the same number of scalar values between all elements to insert', () => {
   const wrongNumberOne = SQLBuilder.insert().into('table');
   const wrongNumberTwo = SQLBuilder.insert().into('table');
 
-  expect(wrongNumberOne.values(['one'], ['one', 'two']));
-  expect(wrongNumberTwo.values(['one', 'two', 'three'], ['one'], ['one', 'two']));
+  expect(() => wrongNumberOne.values(['one'], ['one', 'two'])).toThrow(ALL_RECORDS_SAME_LENGTH);
+  expect(() => wrongNumberTwo.values(['one', 'two', 'three'], ['one'], ['one', 'two'])).toThrow(ALL_RECORDS_SAME_LENGTH);
 });
 
 test('query.insert() does not allow nested values (arrays between arrays)', () => {
   const nestedOne = SQLBuilder.insert().into('table');
 
-  expect(nestedOne.values(['one'], [['one', 'two'], ['three', 'four']])).toThrowError(Error('You can not insert nested values'));
+  expect(() => nestedOne.values(['one'], [['one', 'two'], ['three', 'four']])).toThrow(NO_NESTED_VALUES);
 });
